1b2817baea88681a51b1fb6dbb7bb7cae48277cf
baominh2211 <23520925@gm.uit.edu.vn>
2025-12-25 15:12:21 +0700

    fix ProtectRouter.jsx

--- src/components/BlogDash/ProtectedRoute.jsx
+++ src/components/BlogDash/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 export default function ProtectedRoute() {
   const { isAuthenticated } = useAuth();
 
   // If not authenticated, redirect to login
   if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/blogdash" replace />;
   }
 
   // If authenticated, render child routes
